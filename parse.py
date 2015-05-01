1410c4526772624e79bbb7d2919a4ce7749a782d
stacksmash3r <0x4000@mail.ru>
2015-05-01 15:09:39 +0100

    Rewrite using lxml
    I decided to rewrite the script using lxml and figured out what I am
    going to do in the long run. I plan to use python to parse masscan
    results then insert them into a mysql DB then use a little php to make
    it searchable.

--- parse.py
+++ parse.py
@@ -1,34 +1,26 @@
-...
-Author:Ciaran Strutt
-Date:09/02/2015
-Description:Reads masscan .xml file and outputs the host, port and banner. I wrote
-this at 5am so it was just a quick script to make masscans output easier to look at
-...
-import xml.etree.ElementTree as ET
-import sys
+
+#Author:Ciaran Strutt
+#Date:09/02/2015
+#Description:Reads masscan .xml file and outputs the host, port and banner. I wrote
+#this at 5am so it was just a quick script to make masscans output easier to look at
+
+from lxml import etree as elementTree
+
+fileName = ""
+
+fileName = raw_input("Enter filename: ")
+
+treeData = elementTree.parse(fileName)
+
+root = treeData.getroot();
 
 
-def parseinfo(filename):
-        tree = ET.parse(filename) #Create an element tree from the xml file
-        root = tree.getroot() #Grab the root tag of the xml tree
-
-        hosts = tree.iter('address') #Iterate the children of the tag address
-        ports = tree.iter('port') #same as above
-        banners = tree.iter('service') #same as above
-
-        for ip in hosts: #print out each ip,port and banner
-                try:
-                        print "Host: " + hosts.next().attrib['addr']
-                        print "Port: " + ports.next().attrib['portid']
-                        print "Banner: " + banners.next().attrib['banner']
-                except StopIteration: #Just means end of file has been reached
-                        print "EOF"
-
-
-def main():
-        if(len(sys.argv) < 2):
-                print "Usage python parse.py filename"
-parseinfo(sys.argv[1])
-
-if __name__ == '__main__':
-        main()
+for singleElement in root.iter("address","service","port","state"):
+        if singleElement.get("addr") !=  None:
+                print "IP: %s" % singleElement.get("addr")
+        if singleElement.get("portid") != None:
+                print "Port: %s" % singleElement.get("portid")
+        if singleElement.get("state") != None:
+                print "State: %s" % singleElement.get("state")
+        if singleElement.get("banner") !=  None:
+                print "Banner: %s" % singleElement.get("banner")
